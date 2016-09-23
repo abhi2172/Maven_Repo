317ec6e870b90e97a05aa0e168515c353026f6ce
abhi2172 <abhishek.chakraborty@mheducation.com>
2016-09-23 17:28:46 +0530

    Update Parser.java

--- src/main/java/automationframework/Parser.java
+++ src/main/java/automationframework/Parser.java
@@ -9,8 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 public class Parser extends SetUrl {
 
